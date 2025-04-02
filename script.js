8bc513dc3bee853c6658b015519c71ec6f9e8411
Joshua Concepcion <joshuarconcepcion@gmail.com>
2025-04-01 19:16:56 -0700

    Finished functionality, added more css elements and design.

--- script.js
+++ script.js
@@ -37,3 +37,58 @@ function operate(a, op, b) {
     }
 }
 
+const screen = document.querySelector('#screen-container');
+const numButtons = document.querySelectorAll('#number-buttons button');
+const opButton = document.querySelectorAll('.arithmetic-buttons button:not(#equals-button)');
+const equalButton = document.querySelector('#equals-button');
+
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
+let operatorUsed = false;
+let displayed = false;
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonValue = button.textContent;
+        if (displayed) {
+            screen.textContent = '';
+            firstOperand = '';
+            secondOperand = '';
+            operator = '';
+            operatorUsed = false;
+            displayed = false;
+        }
+        screen.textContent += buttonValue;
+        if (!operatorUsed) {
+            firstOperand += buttonValue;
+            console.log(`firstOperand: ${firstOperand}`);
+        } else {
+            secondOperand += buttonValue;
+            console.log(`secondOperand: ${secondOperand}`);
+        }
+    });
+});
+
+opButton.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!operatorUsed && firstOperand) {
+            const buttonOp = button.textContent;
+            screen.textContent += buttonOp;
+            operator = buttonOp;
+            console.log(`operator: ${operator}`);
+            operatorUsed = true;
+        }
+    });
+});
+
+equalButton.addEventListener('click', () => {
+    if(firstOperand && operator && secondOperand) {
+        const a = parseFloat(firstOperand);
+        const b = parseFloat(secondOperand);
+
+        screen.textContent = operate(a, operator, b);
+        console.log(`answer: ${operate(a, operator, b)}`);
+        displayed = true;
+    }
+})
